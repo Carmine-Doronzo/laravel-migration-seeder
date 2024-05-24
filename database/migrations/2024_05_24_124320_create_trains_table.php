ad71b01c5926c81da8b8fa6de2699b3e94f9ec5e
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-05-24 15:26:06 +0200

    ADD new migration

---
+++ database/migrations/2024_05_24_124320_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',50);
+            $table->string('stazione_di_partenza',100);
+            $table->string('stazione_di_arrivo',100);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->integer('codice_treno')->unsigned();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(0);
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
